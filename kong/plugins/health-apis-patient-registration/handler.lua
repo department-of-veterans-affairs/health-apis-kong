062175c4502c9985caa6e0698010f17196f15cec
dbrown <derek.brown@libertyits.com>
2019-04-22 20:53:02 -0400

    Implemented patient registration plugin, replaced 307 redirect

--- kong/plugins/health-apis-patient-registration/handler.lua
+++ kong/plugins/health-apis-patient-registration/handler.lua
@@ -1,7 +1,35 @@
 local HealthApisPatientRegistration = require("kong.plugins.base_plugin"):extend()
 
+local http = require "resty.http"
+local cjson = require "cjson.safe"
+
+local find = string.find
+local format = string.format
+
+local OPERATIONAL_OUTCOME_TEMPLATE =
+  '{ "resourceType": "OperationOutcome",\n' ..
+  '  "id": "exception",\n' ..
+  '  "text": {\n' ..
+  '      "status": "additional",\n' ..
+  '      "div": "<div xmlns=\\"http://www.w3.org/1999/xhtml\\"><p>%s</p></div>"\n' ..
+  '  },\n' ..
+  '  "issue": [\n' ..
+  '      {\n' ..
+  '          "severity": "error",\n' ..
+  '          "code": "exception",\n' ..
+  '          "details": {\n' ..
+  '              "text": "%s"\n' ..
+  '          }\n' ..
+  '      }\n' ..
+  '  ]\n' ..
+  '}'
+
+local BAD_AUTHORIZE_RESPONSE = "Authorization failed."
+local BAD_IDS_RESPONSE = "IDS failed."
+
+
 function HealthApisPatientRegistration:new()
-  HealthApisPatientRegistration.super.new(self, "health-apis-static-token-handler")
+  HealthApisPatientRegistration.super.new(self, "health-apis-patient-registration")
 end
 
 function HealthApisPatientRegistration:access(conf)
@@ -17,24 +45,112 @@ function HealthApisPatientRegistration:access(conf)
   -- Required by lua (request body data not loaded by default)
   ngx.req.read_body()
 
-  local body, errors = ngx.req.get_post_args()
+  local body_data, errors = ngx.req.get_body_data()
 
   if errors == "truncated" then
     -- one can choose to ignore or reject the current request here
   end
 
-  if not body then
+  if not body_data then
     ngx.log(ngx.ERR, "failed to get post args: ", errors)
     return
   end
+    
+  local client = http.new()
+  client:set_timeout(self.conf.token_timeout)
+    
+  local token_res, err = client:request_uri(self.conf.token_url, {
+    method = "POST",
+    ssl_verify = false,
+    headers = {
+      ["Content-Type"] = "application/x-www-form-urlencoded",
+    },
+    body = body_data,
+  })
+  
+  if not token_res then
+    -- Error making request to validate endpoint
+    return self:send_fhir_response(404, BAD_AUTHORIZE_RESPONSE)
+  end
 
-  --Retrieve ICN
-  --Register with IDS
+  -- Get the status and body of the token request
+  -- So we can be done with the connection
+  local token_res_status = token_res.status
+  local token_res_body = token_res.body
 
+  -- If unauthorized, we block the user
+  if (token_res_status == 401) then
+    return self:send_fhir_response(401, BAD_AUTHORIZE_RESPONSE)
+  end
+
+  -- An unexpected condition
+  if (token_res_status < 200 or token_res_status > 299) then
+    return self:send_fhir_response(500, BAD_AUTHORIZE_RESPONSE)
+  end
+
+  local token_res_json = cjson.decode(token_res_body)      
+  self:register_patient(token_res_json.patient)
+  
+  return self:send_response(token_res_status, token_res_body)
+
+end
+
+function HealthApisPatientRegistration:register_patient(patient_icn)
+
+  local ids_client = http.new()
+  ids_client:set_timeout(self.conf.token_timeout)
+  
+  local ids_body_data = '[\n' ..
+    '{\n' ..
+    '    "system": "CDW",\n' ..
+    '    "resource": "PATIENT",\n' ..
+    '    "identifier": "' .. patient_icn .. '"\n' ..
+    '}\n' ..
+    ']'
+  
+  local ids_res, err = ids_client:request_uri(self.conf.ids_url, {
+    method = "POST",
+    ssl_verify = false,
+    headers = {
+      ["Content-Type"] = "application/json",
+    },
+    body = ids_body_data,
+  })
+  
+  if not ids_res then
+    -- Error making request to validate endpoint
+    return self:send_fhir_response(404, BAD_IDS_RESPONSE)
+  end
+
+  -- Get the status and body of the verification request
+  -- So we can be done with the connection
+  local ids_res_status = ids_res.status
+  local ids_res_body = ids_res.body
+
+  -- If unauthorized, we block the user
+  if (ids_res_status == 401) then
+    return self:send_fhir_response(401, BAD_IDS_RESPONSE)
+  end
+
+  -- An unexpected condition
+  if (ids_res_status < 200 or ids_res_status > 299) then
+    return self:send_fhir_response(500, BAD_IDS_RESPONSE)
+  end
+  
 end
 
 
 -- Format and send the response to the client
+function HealthApisPatientRegistration:send_fhir_response(status_code, message)
+
+  ngx.status = status_code
+  ngx.header["Content-Type"] = "application/json"
+
+  ngx.say(format(OPERATIONAL_OUTCOME_TEMPLATE, message, message))
+
+  ngx.exit(status_code)
+end
+
 function HealthApisPatientRegistration:send_response(status_code, message)
 
   ngx.status = status_code
--- kong/plugins/health-apis-patient-registration/schema.lua
+++ kong/plugins/health-apis-patient-registration/schema.lua
@@ -1,6 +1,8 @@
 return {
   no_consumer = true,
   fields = {
-    ids_url = {type = "string"}
+    ids_url = {type = "string"},
+    token_url = {type = "string"},
+    token_timeout = {type = "number", default = 10000},
   }
 }
